f4a069417ae1c685e410b72ef2312a7c615fe414
Usuario <Usuario@DESKTOP-5V8UOOD>
2024-04-08 22:43:15 +0200

    Inicio total

---
+++ src/main/java/com/mycompany/tareaentornos/TareaEntornos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareaentornos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TareaEntornos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
